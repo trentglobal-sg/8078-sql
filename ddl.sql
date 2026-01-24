042c0c37307638f05aded1211608ef8cb0166667
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-01-24 04:10:04 +0000

    Added DDL examples

--- ddl.sql
+++ ddl.sql
@@ -27,3 +27,48 @@ SHOW TABLES;
 
 -- see the columns in one specific tale
 DESCRIBE parents;
+
+CREATE TABLE locations (
+    location_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL
+) ENGINE = INNODB;
+
+-- row manipulations is done by DML 
+-- add a new row: INSERT INTO <table> (<col1>, <col2>) VALUES (value1, value2...)
+INSERT INTO parents (first_name, last_name) VALUES ("Mary", "Sue");
+
+-- show all rows from a table (DQL)
+SELECT * FROM parents;
+
+-- To create a table with FK
+-- Step 1: Create the students table
+CREATE TABLE students (
+    student_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45),
+    swimming_level TINYINT UNSIGNED,
+    dob DATETIME NOT NULL,
+    parent_id INT UNSIGNED NOT NULL
+) ENGINE = INNODB;
+
+ALTER TABLE students
+    ADD CONSTRAINT fk_parents_students
+    FOREIGN KEY (parent_id) REFERENCES parents(parent_id);
+
+INSERT INTO students (first_name, last_name, swimming_level, dob, parent_id)
+ VALUES ("John", "Sue", 1, "2022-01-24", 1);
+
+ INSERT INTO students (first_name, last_name, swimming_level, dob, parent_id)
+ VALUES ("Peter", "Barker", 10, "1990-01-24", 99);
+
+ -- remove a column from a table
+ ALTER TABLE students DROP COLUMN swimming_level;
+
+ -- add a column
+ -- add an email address column to the parents
+ ALTER TABLE parents ADD COLUMN email VARCHAR(255);
+
+ -- altering a column
+ -- eg: make the email column in the parents table NOT NULL (i.e complusory)
+ ALTER TABLE students MODIFY COLUMN last_name VARCHAR(100);
